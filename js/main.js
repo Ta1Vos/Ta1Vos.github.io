5317ad93339b25b6a6cd02d5683600b4db1d8065
Ta1Vos <302196386@student.rocmondriaan.nl>
2023-06-02 10:03:20 +0200

    Added automatic div resizing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 document.body.style.backgroundImage = `linear-gradient(${Math.round(Math.random(360)) * 100}deg, #FF8552, rgba(228, 225, 219, 1))`;
+
+const projectCards = document.querySelectorAll('.project-card');
+
+projectCards.forEach(card => {
+    const text = card.querySelector('.card-text');
+
+    card.addEventListener('mouseenter', function() {
+        text.classList.remove('shrinked-div');
+    })
+
+    card.addEventListener('mouseleave', function() {
+        text.classList.add('shrinked-div');
+    })
+
+    text.classList.add('shrinked-div');
+});
